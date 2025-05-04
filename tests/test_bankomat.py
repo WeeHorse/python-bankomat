f72a54e92a6a2d06a906e36d859b411534b27d39
Benjamin Berglund <benjaminberglund@gmail.com>
2025-05-04 23:44:04 +0200

    adjusted tests for refactored code with actual bills

--- tests/test_bankomat.py
+++ tests/test_bankomat.py
@@ -45,56 +45,53 @@ def test_enter_valid_pin():
     result = bankomat.enter_pin("0123")
     assert result == True
 
-def test_withdraw_1000():
-    # testdata
-    initial_machine_balance = 11000
-    initial_account_balance = 1000
-    withdrawal = 1000
-    # setup
-    bankomat = Bankomat()
-    bankomat.machine_balance = initial_machine_balance
-    account = Account("Benjamin", "Berglund", "700109-2456", initial_account_balance)
-    card = Card(account)
-    bankomat.insert_card(card)
-    bankomat.init_card("0123")
-    bankomat.enter_pin("0123")
-    # test
-    result = bankomat.withdraw(withdrawal)
-    assert result == withdrawal
-    assert account.balance == initial_account_balance - withdrawal
-    assert bankomat.machine_balance + result == initial_machine_balance
-
-
-# Example using a  fixture (helper method to avoid all this repetitive code)
+# Fixture (helper method to avoid all this repetitive code)
 @pytest.fixture
 def banko():
     bankomat = Bankomat()
-    account = Account("Benjamin", "Berglund", "700109-2456", 6000)
+    bankomat.add_cash(100,50)
+    bankomat.add_cash(200, 20)
+    bankomat.add_cash(500, 4)
+    account = Account("Benjamin", "Berglund", "700109-2456")
+    account.deposit(6000)
     card = Card(account)
     bankomat.insert_card(card)
     bankomat.init_card("0123")
     bankomat.enter_pin("0123")
     return bankomat
 
+def test_withdraw_1000(banko):
+    # setup
+    withdrawal = 1000
+    initial_machine_balance = banko.machine_balance()
+    account = banko.card.account
+    initial_account_balance = account.get_balance()
+    # test
+    result = banko.withdraw(withdrawal)
+    assert result == withdrawal
+    assert initial_account_balance == account.get_balance() + withdrawal
+    assert banko.machine_balance() + result == initial_machine_balance
+
 def test_withdraw_all(banko):
     result = banko.withdraw(6000)
     assert result == 6000
-    assert banko.card.account.balance == 0
+    assert banko.card.account.get_balance() == 0
 
 def test_withdraw_half(banko):
     result = banko.withdraw(3000)
     assert result == 3000
-    assert banko.card.account.balance == 3000
+    assert banko.card.account.get_balance() == 3000
 
 def test_overdraw_account(banko):
     result = banko.withdraw(7000)
     assert result == 0
-    assert banko.card.account.balance == 6000
+    assert banko.card.account.get_balance() == 6000
 
 def test_overdraw_bankomat(banko):
-    # special, set lower amount left in machine
-    banko.machine_balance = 5000
-    result = banko.withdraw(6000)
+    # setup (increase account to 12000)
+    banko.card.account.deposit(6000)
+    # test
+    result = banko.withdraw(12000)
     assert result == 0
-    assert banko.machine_balance == 5000
-    assert banko.card.account.balance == 6000
+    assert banko.machine_balance() == 11000
+    assert banko.card.account.get_balance() == 12000
