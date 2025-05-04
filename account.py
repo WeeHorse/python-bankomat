9cc0c09ee63d6ebb7f969f1423d130150e632ca9
Benjamin Berglund <benjaminberglund@gmail.com>
2025-05-04 23:56:14 +0200

    added account nr

--- account.py
+++ account.py
@@ -1,9 +1,19 @@
+import random
+
+
 class Account:
-    def __init__(self, first_name, last_name, ssn, balance=0):
-        self.balance = balance
+    def __init__(self, first_name, last_name, ssn):
+        self.balance = 0
         self.first_name = first_name
         self.last_name = last_name
         self.ssn = ssn
+        self.account_nr = self.generateAccountNr()
+
+    def generateAccountNr(self):
+        return "".join(random.choices('0123456789', k=10))
+
+    def get_account_nr(self):
+        return self.account_nr
 
     def withdraw(self, amount):
         if amount > 0 and self.balance >= amount:
--- tests/test_account.py
+++ tests/test_account.py
@@ -4,6 +4,7 @@ from account import Account
 def test_account_creation():
     account = Account("Benjamin", "Berglund", "700109-2456")
     assert len(account.ssn) == 11 or len(account.ssn) == 13
+    assert len(account.get_account_nr()) == 10
 
 def test_deposit_1000():
     account = Account("Benjamin", "Berglund", "700109-2456")
